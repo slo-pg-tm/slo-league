9deb6d45ca90c84b839c07d5d3a10f6e14055f48
Primoz Susa <primoz.susa@gmail.com>
2018-05-01 16:30:15 +0200

    first 2018 calculation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ const jetpack = require('fs-jetpack');
 const getCompsData = require('./liga/getCompsData');
 const calcLiga = require('./liga/calcLiga');
 
-const compsDir = path.join(__dirname, './data/2017');
+// const compsDir = path.join(__dirname, './data/2017');
+// const compsData = getCompsData(compsDir);
+// const ligaData = calcLiga(compsData);
+// jetpack.write(path.join(__dirname, './data/liga-2017.json'), ligaData);
+
+const compsDir = path.join(__dirname, './data/2018');
 const compsData = getCompsData(compsDir);
 const ligaData = calcLiga(compsData);
-jetpack.write(path.join(__dirname, './data/liga-2017.json'), ligaData);
+jetpack.write(path.join(__dirname, './data/liga-2018.json'), ligaData);
